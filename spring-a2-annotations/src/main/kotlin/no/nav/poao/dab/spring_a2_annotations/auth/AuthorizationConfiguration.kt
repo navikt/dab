eaabd008ff9b286940488ff409787304059da9b3
johannetronstad <johanne.tronstad@nav.no>
2023-08-31 12:48:46 +0200

    autoconfiguration

--- spring-a2-annotations/src/main/kotlin/no/nav/poao/dab/spring_a2_annotations/auth/AuthorizationConfiguration.kt
+++ spring-a2-annotations/src/main/kotlin/no/nav/poao/dab/spring_a2_annotations/auth/AuthorizationConfiguration.kt
@@ -1,16 +1,19 @@
 package no.nav.poao.dab.spring_a2_annotations.auth
 
-import org.springframework.beans.factory.annotation.Autowired
+import no.nav.poao.dab.spring_auth.AuthService
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-open class AuthorizationConfiguration : WebMvcConfigurer {
+open class AuthorizationConfiguration {
 
-    @Autowired
-    lateinit var authorizationInterceptor: AuthorizationInterceptor
-    override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(authorizationInterceptor)
+    @Bean
+    open fun authorizationInterceptor(authService: AuthService): AuthorizationInterceptor {
+        return AuthorizationInterceptor(authService)
+    }
+
+    @Bean
+    open fun authorizationWebMvcConfigurer(): AuthorizationWebMvcConfigurer {
+        return AuthorizationWebMvcConfigurer()
     }
 }
