b4bae64572d17431b93d4e5ff116a015fffbc0b0
haakonph <hakon.monn-holm@nav.no>
2023-10-12 09:32:03 +0200

    fiks internbruker auth

--- spring-auth/src/main/kotlin/no/nav/poao/dab/spring_auth/InternBrukerAuth.kt
+++ spring-auth/src/main/kotlin/no/nav/poao/dab/spring_auth/InternBrukerAuth.kt
@@ -9,11 +9,16 @@ import java.util.*
 
 internal class InternBrukerAuth(private val pep: PoaoTilgangClient, private val personService: IPersonService) {
     fun sjekkInternbrukerHarSkriveTilgangTilPerson(azureNavId: UUID, aktorId: EksternBrukerId, navIdent: NavIdent): Unit = harTilgang(azureNavId,  aktorId, TilgangType.SKRIVE, navIdent).throwIfIkkeTilgang()
-    fun harInternbrukerHarLeseTilgangTilPerson(azureNavId: UUID, aktorId: EksternBrukerId, navIdent: NavIdent): Result = harTilgang(azureNavId,  aktorId, TilgangType.LESE, navIdent)
-    fun harTilgang(azureNavId: UUID, aktorId: EksternBrukerId, actionId: TilgangType, navIdent: NavIdent): Result {
+    fun harInternbrukerHarLeseTilgangTilPerson(azureNavId: UUID, aktorId: EksternBrukerId, navIdent: NavIdent):  AuthResult = harTilgang(azureNavId,  aktorId, TilgangType.LESE, navIdent)
+    fun harTilgang(azureNavId: UUID, aktorId: EksternBrukerId, actionId: TilgangType, navIdent: NavIdent): AuthResult {
         val fnr = personService.getFnrForAktorId(aktorId).get()
         val evaluatePolicy = pep.evaluatePolicy(NavAnsattTilgangTilEksternBrukerPolicyInput(azureNavId, actionId, fnr))
         val harTilgang = evaluatePolicy.get()?.isPermit ?: false
-        return Result(harTilgang= harTilgang, accesedIdnet = aktorId, byIdent= navIdent, melding = null)
+
+        return if(harTilgang) {
+            AuthResult.UserSuccessResult(subjectIdent = navIdent, objectIdent = aktorId)
+        } else {
+            AuthResult.UserFailedResult(subjectIdent = navIdent, objectIdent =  aktorId, "")
+        }
     }
 }
