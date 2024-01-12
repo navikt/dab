055aa730635d3dc4df5f6631bb2fba45dbe802ba
Sigurd Grøneng <sigurd.groneng@nav.no>
2024-01-12 10:44:08 +0100

    Require not blank (#25)

--- ktor-oauth-client/src/main/kotlin/no/nav/poao/dab/ktor_oauth_client/OauthConfig.kt
+++ ktor-oauth-client/src/main/kotlin/no/nav/poao/dab/ktor_oauth_client/OauthConfig.kt
@@ -1,6 +1,14 @@
 package no.nav.poao.dab.ktor_oauth_client
 
+
+typealias ClientSecret = String
 data class OauthClientCredentialsConfig(
     val clientId: String,
-    val clientSecret: String,
-    val tokenEndpoint: String)
+    val clientSecret: ClientSecret,
+    val tokenEndpoint: String) {
+    init {
+        require(clientId.isNotBlank()) { "ClientId can not be null" }
+        require(clientSecret.isNotBlank()){ "ClientSecret can not be null" }
+        require(tokenEndpoint.isNotBlank()) { "TokenEndpoint can not be null" }
+    }
+}
