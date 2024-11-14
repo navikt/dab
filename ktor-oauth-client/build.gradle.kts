2a4966b86f927eb9e10c981678eae7d44d678904
Mads Lee Giil <mads.lee.giil@nav.no>
2024-11-14 10:36:51 +0100

    Oppgrader Ktor og Kotlin

--- ktor-oauth-client/build.gradle.kts
+++ ktor-oauth-client/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("dab.kotlin-libary")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 repositories {
